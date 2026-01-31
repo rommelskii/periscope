c9dee5c5b2c805a7f619645a060ce5375dec90a3
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-01-31 20:55:05 +0800

    [log] added magic number

--- include/log.h
+++ include/log.h
@@ -6,11 +6,15 @@
  * Contains the definition of the Periscope log type and its public API
  **********************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /****************************************
  * Constants
  ****************************************/
 #define MACADDRLEN 6
 #define MAXMSGLEN 2048
+#define MAGIC_NUBMER 0xDEADB33F
 
 
 /****************************************
@@ -25,6 +29,7 @@ typedef enum log_type
 
 typedef struct 
 {
+  uint32_t    magic_number;
   uint32_t    src_ip;
   uint8_t     src_mac[MACADDRLEN];
   log_type_t  type; 
@@ -40,25 +45,5 @@ typedef struct
   * @param plog Pointer to log to be printed
   */
 void print_log (const log_t* plog);
-
-/**
-  * @brief Serializes a log to a byte buffer
-  *
-  * @param plog Pointer to the log to be serialized
-  * @param buf Destination byte array
-  * @param maxsize Maximum length to be serialized
-  *
-  * @return 0 if success. -1 if failure
-  */
-int serialize_log(log_t* plog, uint8_t* buf, size_t maxsize);
-
-/**
-  * @brief Unpacks a byte stream to a log type
-  * @param plog Destination log variable
-  * @param buf Source byte array to be deserialized
-  *
-  * @return 0 if success. -1 if failure
-  */
-int deserialize_log(log_t* plog, uint8_t* buf);
 
 #endif //LOG_H_
