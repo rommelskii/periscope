28f26193643c6701fd2e508f9f32b908b10639ea
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-01 03:43:02 +0800

    [log serialization] changed serialization methods
    turned them into void instead of int since they don't really have to
    return an integer to indicate a success ()

--- include/log_serialize.h
+++ include/log_serialize.h
@@ -9,17 +9,14 @@
   * @param plog Pointer to the log to be serialized
   * @param buf Destination byte array
   * @param maxsize Maximum length to be serialized
-  *
-  * @return 0 if success. -1 if failure
   */
-int serialize_log(log_t* plog, uint8_t* buf, size_t maxsize);
+void serialize_log(log_t* plog, uint8_t* buf, size_t maxsize);
 
 /**
   * @brief Unpacks a byte stream to a log type
   * @param plog Destination log variable
   * @param buf Source byte array to be deserialized
-  *
-  * @return 0 if success. -1 if failure
   */
-int deserialize_log(log_t* plog, uint8_t* buf);
+void deserialize_log(log_t* plog, uint8_t* buf);
+
 #endif //LOG_SERIALIZE_H_
--- src/log_serialize.c
+++ src/log_serialize.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "log.h"
 #include "log_serialize.h"
 
-int serialize_log(log_t* plog, uint8_t* buf, size_t maxsize) 
+void serialize_log(log_t* plog, uint8_t* buf, size_t maxsize) 
 {
-  return 0;
+  memset(buf, 0, maxsize);
+  memcpy(buf, plog, sizeof(log_t));
+  return;
 }
 
-int deserialize_log(log_t* plog, uint8_t* buf) 
+void deserialize_log(log_t* plog, uint8_t* buf) 
 {
-  return 0;
+  plog->magic_number = (uint32_t)buf[0] << 24 | (uint32_t)buf[1] << 16 | (uint32_t)buf[2] << 8 | (uint32_t)buf[3];
+  plog->src_ip = (uint32_t)buf[7] << 24 | (uint32_t)buf[6] << 16 | (uint32_t)buf[5] << 8 | (uint32_t)buf[4];
+
+  memcpy(plog->src_mac, &buf[8], MACADDRLEN);
+
+  uint8_t temp_type = buf[8 + MACADDRLEN]; // 8 = magic num len + ip len
+  plog->type = (log_type_t)temp_type;
+
+  memcpy(plog->content, &buf[8 + MACADDRLEN + sizeof(log_type_t)], MAXMSGLEN);
+
+  return;
 }
