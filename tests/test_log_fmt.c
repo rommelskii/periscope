ce6cfdf20b12d374a72bfa68bf0c68b14617a216
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-01 19:15:14 +0800

    [test] added log fmt tests

---
+++ tests/test_log_fmt.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "log.h"
+#include "testing_utils.h" //< for EXPECT
+ 
+/**
+  * file: test_log.c
+  * Unit test for the log data type
+  */
+
+/**
+ * GLOBAL VARIABLES
+ */
+int tests_failed = 0; //< necessary for testing library
+
+int main() 
+{
+  /** 
+   * TEST PARAMETERS
+   */
+  char* actual_mac = "de:ab:cd:ef:fe:ee";
+  char* actual_type = "CRITICAL";
+
+  /**
+   * TEST INITIALIZATION 
+   */
+  uint8_t test_mac[6] = {0xDE, 0xAB, 0xCD, 0xEF, 0xFE, 0xEE};
+  log_type_t test_type = CRITICAL;
+
+  char* result_mac = log_mac_to_string(test_mac);
+  char* result_type = log_type_to_string(test_type);
+
+  /**
+   * TEST PROPER
+   */
+  printf("----LOG FORMATTER TESTS----\n");
+  EXPECT(memcmp(result_mac, actual_mac, MACADDRLEN) == 0, "MAC formatter test");
+  EXPECT(result_type == actual_type, "Log type formatter test");
+
+  if (tests_failed > 0)
+  {
+    printf("%s: One or more tests have failed\n", __FILENAME__);
+    return 1;
+  }
+}
+
