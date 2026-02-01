3b08fb90bdb2fcba37e18f3fe1ea9a05b616bea2
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-01 20:00:44 +0800

    [server] added debug info for loaded file

--- src/psc_server.c
+++ src/psc_server.c
@@ -36,6 +36,25 @@ int main(void)
 
   printf("Running Periscope server at port %d\n", PSC_PORT);
 
+  char cbuf[FILE_MAXSIZE]; 
+  memset(cbuf, 0, sizeof(cbuf));
+
+  FILE* pconf = fopen(FILE_CONF_PATH, "r");
+  if (pconf == NULL)
+  {
+    fprintf(stderr, "File configuration error: cannot load configuration file\n");
+  }
+
+  fgets(cbuf, sizeof(cbuf), pconf);
+  if(strnlen(cbuf, FILE_MAXSIZE) == 0) 
+  {
+    fprintf(stderr, "File configuration error: empty configuration file\n");
+    return 1;
+  }
+
+  printf("Configuration file loaded from %s\n", FILE_CONF_PATH);
+  printf("Logging file loaded from %s\n", cbuf);
+  
   for (;;) 
   {
     int bytes_received = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&cli, &addrlen);
