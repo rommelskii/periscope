457ca0fee2e727c06ec64c76adb14aceca88f883
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-01 17:01:33 +0800

    [project] modified testing script for exiting to failure

--- run_tests.sh
+++ run_tests.sh
@@ -8,7 +8,13 @@ for test_file in ./test_*; do
     if [ -x "$test_file" ]; then
         echo "Running: $test_file"
         "$test_file"
+        if [ $? -eq 1 ]; then 
+          echo "run_tests.sh: one or more tests have failed! Exiting..."
+          exit 1
+        fi
     else
         echo "Skipping: $test_file (not executable)"
     fi
 done
+
+exit 0
