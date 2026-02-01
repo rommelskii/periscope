11eb0da20392864c8848a5c915c36a18701b66f1
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-01 16:39:54 +0800

    [test] added macro for shortening filepath

--- tests/test_log.c
+++ tests/test_log.c
@@ -15,7 +15,15 @@
   * Unit test for the log data type
   */
 
-int tests_failed = 0;
+/**
+ * MACROS
+ */
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) 
+
+/**
+ * GLOBAL VARIABLES
+ */
+int tests_failed = 0; //< necessary for testing library
 
 int main() 
 {
@@ -44,44 +52,15 @@ int main()
   EXPECT(test_log.type == test_type, "type test");
   EXPECT(memcmp(test_log.content, test_content, strnlen(test_content, MAXMSGLEN)+1) == 0, "content test");
 
+  print_log(&test_log); //< test print
+  
+  printf("\nAttempting to print malformed log...\n");
+  test_log.magic_number = 0x00000000;
+  print_log(&test_log);
+
   if (tests_failed > 0)
   {
-    printf("%s: One or more tests have failed\n", __FILE__);
+    printf("%s: One or more tests have failed\n", __FILENAME__);
     return 1;
   }
 }
-
-/*
-int main()
-{
-  printf("[Test] Log datatype... ");
-  log_t log = {0};
-
-  uint8_t test_mac[6] = {0xDE, 0xEA, 0xDD, 0xBE, 0xEE, 0xFF};
-  uint32_t magic_number = 0xdeadbeef;
-  uint32_t test_ip = inet_addr("127.0.0.1");
-
-  log_type_t test_type = STANDARD;
-
-
-  log.magic_number = magic_number;
-  log.src_ip = test_ip;
-  memcpy(log.src_mac, test_mac, MACADDRLEN);
-  log.type = test_type;
-  memcpy(log.content, test_content, strlen(test_content)+1);
-  
-  assert(log.magic_number == magic_number);
-  assert(log.src_ip == test_ip);
-
-  for (size_t i = 0; i < MACADDRLEN; ++i) 
-  {
-    assert(log.src_mac[i] == test_mac[i]);
-  }
-
-  assert(log.type == test_type);
-
-  assert(strcmp(log.content, test_content) == 0);
-
-  printf("PASSED\n");
-}
-*/
