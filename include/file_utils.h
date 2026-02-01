0d332add40e7ff3f2b824dff29182eb0b66f118b
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-01 18:04:06 +0800

    [file] added header file boilerplate for file management

---
+++ include/file_utils.h
@@ -0,0 +1,11 @@
+#ifndef FILE_UTILS_H_
+#define FILE_UTILS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <log.h>
+
+int file_log_process(log_t* plog);
+
+#endif //FILE_UTILS_H_
---
+++ src/file_utils.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "log.h"
+#include "file_utils.h"
+
+int file_log_process(log_t* plog) 
+{
+  if (plog == NULL) 
+  {
+    return -1;
+  }
+  return 0;
+}
